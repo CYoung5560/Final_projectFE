bf676a3efd1e81c05e58114cc94fb888e1f5d429
shivalasvegas <shiva.king@academytrainee.com>
2020-08-27 17:42:41 +0100

    SK - Added Signup.css

--- frontend/src/GalleryPage.js
+++ frontend/src/GalleryPage.js
@@ -1,14 +1,159 @@
 import React from "react";
 import "./App.css";
-import axios from 'axios';
-
 export default class PersonList extends React.Component {
   
   render() {
     return (
       
       <div>
-        Welcome to the Gallery Page
+        <div class="album py-5 bg-light">
+        <div class="container">
+    
+          <div class="row">
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <img src="/html/pix/ivor_sideleft_500px.jpg"  width="100%" height="100%" class = "img-responsive" ></img>
+                <div class="card-body">
+                  <p class="card-text">Ivor an Original Feltoon.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-secondary">Buy on Etsy</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <img src="/html/pix/roberta_front_500px.jpg"  width="100%" height="100%" class = "img-responsive"></img>
+                <div class="card-body">
+                  <p class="card-text">Roberta an Original Feltoon</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-secondary">Buy on Etsy</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <img src="/html/pix/stanley_front_500px.jpg"  width="100%" height="100%" class = "img-responsive" ></img>
+                <div class="card-body">
+                  <p class="card-text">Stanley an Original Feltoon</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-secondary">Buy on Etsy</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+    
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <img src="/html/pix/yingYue_front_500px.jpg"  width="100%" height="100%" class = "img-responsive" ></img>
+                <div class="card-body">
+                  <p class="card-text">Ying Yue an Orginal Feltoon</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-secondary">Buy on Etsy</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <img src="/html/pix/daisy_sideright_500px.jpg"  width="100%" height="100%" class = "img-responsive" ></img>
+                <div class="card-body">
+                  <p class="card-text">Daisy an Orignal Feltoon</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-secondary">Buy on Etsy</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <img src="/html/pix/doris_sideleft_500px.jpg"  width="100%" height="100%" class = "img-responsive" ></img>
+                <div class="card-body">
+                  <p class="card-text">Doris an Original Feltoon</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-secondary">Buy on Etsy</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+    
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <img src="/html/pix/mrGrey_500px.jpg"  width="100%" height="100%" class = "img-responsive"></img>
+                <div class="card-body">
+                  <p class="card-text">Mr Grey Comes to Tea: from The Gorgeous Bunch</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-secondary">Coming soon!</button>
+                    </div>
+                  
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <img src="/html/pix/mrPip_500px.jpg"  width="100%" height="100%" class = "img-responsive" ></img>
+                <div class="card-body">
+                  <p class="card-text">Mr Pip Walks to Town: from The Gorgeous Bunch</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-secondary">Coming soon!</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+              <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                  <img src="/html/pix/mrMidnight_500px.jpg"  width="100%" height="100%" class = "img-responsive" ></img>
+                  <div class="card-body">
+                    <p class="card-text">Mr Midnight Dodges the Hunt: from The Gorgeous Bunch</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-secondary">Coming soon!</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+           
+
+              <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                  <img src="/html/pix/drBabbit_500px.jpg"  width="100%" height="100%" class = "img-responsive" ></img>
+                  <div class="card-body">
+                    <p class="card-text">Dr Babbit Races to Lunch: from The Gorgeous Bunch</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-secondary">Coming soon!</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              </div>
+              </div>
+              </div>
       </div>
     )
   }
