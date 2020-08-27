9ea02748d8bcf0850609f24f445dbe681c9c2704
shivalasvegas <shiva.king@academytrainee.com>
2020-08-27 17:49:57 +0100

    SK - Added cards to gallery

--- frontend/src/GalleryPage.js
+++ frontend/src/GalleryPage.js
@@ -12,12 +12,12 @@ export default class PersonList extends React.Component {
           <div class="row">
             <div class="col-md-4">
               <div class="card mb-4 shadow-sm">
-                <img src="/html/pix/ivor_sideleft_500px.jpg"  width="100%" height="100%" class = "img-responsive" ></img>
+                <img src=""  width="100%" height="100%" class = "img-responsive" ></img>
                 <div class="card-body">
-                  <p class="card-text">Ivor an Original Feltoon.</p>
+                  <p class="card-text">Movie</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-secondary">Buy on Etsy</button>
+                      <button type="button" class="btn btn-sm btn-secondary">Coming soon!</button>
                     </div>
                   </div>
                 </div>
@@ -26,12 +26,12 @@ export default class PersonList extends React.Component {
 
             <div class="col-md-4">
               <div class="card mb-4 shadow-sm">
-                <img src="/html/pix/roberta_front_500px.jpg"  width="100%" height="100%" class = "img-responsive"></img>
+                <img src=""  width="100%" height="100%" class = "img-responsive"></img>
                 <div class="card-body">
-                  <p class="card-text">Roberta an Original Feltoon</p>
+                  <p class="card-text">Movie</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-secondary">Buy on Etsy</button>
+                      <button type="button" class="btn btn-sm btn-secondary">Coming soon!</button>
                     </div>
                   </div>
                 </div>
@@ -40,12 +40,12 @@ export default class PersonList extends React.Component {
 
             <div class="col-md-4">
               <div class="card mb-4 shadow-sm">
-                <img src="/html/pix/stanley_front_500px.jpg"  width="100%" height="100%" class = "img-responsive" ></img>
+                <img src=""  width="100%" height="100%" class = "img-responsive" ></img>
                 <div class="card-body">
-                  <p class="card-text">Stanley an Original Feltoon</p>
+                  <p class="card-text">Movie</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-secondary">Buy on Etsy</button>
+                      <button type="button" class="btn btn-sm btn-secondary">Coming soon!</button>
                     </div>
                   </div>
                 </div>
@@ -54,12 +54,12 @@ export default class PersonList extends React.Component {
     
             <div class="col-md-4">
               <div class="card mb-4 shadow-sm">
-                <img src="/html/pix/yingYue_front_500px.jpg"  width="100%" height="100%" class = "img-responsive" ></img>
+                <img src=""  width="100%" height="100%" class = "img-responsive" ></img>
                 <div class="card-body">
-                  <p class="card-text">Ying Yue an Orginal Feltoon</p>
+                  <p class="card-text">Movie</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-secondary">Buy on Etsy</button>
+                      <button type="button" class="btn btn-sm btn-secondary">Coming soon!</button>
                     </div>
                   </div>
                 </div>
@@ -68,12 +68,12 @@ export default class PersonList extends React.Component {
 
             <div class="col-md-4">
               <div class="card mb-4 shadow-sm">
-                <img src="/html/pix/daisy_sideright_500px.jpg"  width="100%" height="100%" class = "img-responsive" ></img>
+                <img src=""  width="100%" height="100%" class = "img-responsive" ></img>
                 <div class="card-body">
-                  <p class="card-text">Daisy an Orignal Feltoon</p>
+                  <p class="card-text">Movie</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-secondary">Buy on Etsy</button>
+                      <button type="button" class="btn btn-sm btn-secondary">Coming soon!</button>
                     </div>
                   </div>
                 </div>
@@ -82,12 +82,12 @@ export default class PersonList extends React.Component {
 
             <div class="col-md-4">
               <div class="card mb-4 shadow-sm">
-                <img src="/html/pix/doris_sideleft_500px.jpg"  width="100%" height="100%" class = "img-responsive" ></img>
+                <img src=""  width="100%" height="100%" class = "img-responsive" ></img>
                 <div class="card-body">
-                  <p class="card-text">Doris an Original Feltoon</p>
+                  <p class="card-text">Movie</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-secondary">Buy on Etsy</button>
+                      <button type="button" class="btn btn-sm btn-secondary">Coming soon!</button>
                     </div>
                   </div>
                 </div>
@@ -96,9 +96,9 @@ export default class PersonList extends React.Component {
     
             <div class="col-md-4">
               <div class="card mb-4 shadow-sm">
-                <img src="/html/pix/mrGrey_500px.jpg"  width="100%" height="100%" class = "img-responsive"></img>
+                <img src=""  width="100%" height="100%" class = "img-responsive"></img>
                 <div class="card-body">
-                  <p class="card-text">Mr Grey Comes to Tea: from The Gorgeous Bunch</p>
+                  <p class="card-text">Movie</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
                       <button type="button" class="btn btn-sm btn-secondary">Coming soon!</button>
@@ -111,9 +111,9 @@ export default class PersonList extends React.Component {
 
             <div class="col-md-4">
               <div class="card mb-4 shadow-sm">
-                <img src="/html/pix/mrPip_500px.jpg"  width="100%" height="100%" class = "img-responsive" ></img>
+                <img src=""  width="100%" height="100%" class = "img-responsive" ></img>
                 <div class="card-body">
-                  <p class="card-text">Mr Pip Walks to Town: from The Gorgeous Bunch</p>
+                  <p class="card-text">Movie</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
                       <button type="button" class="btn btn-sm btn-secondary">Coming soon!</button>
@@ -125,9 +125,9 @@ export default class PersonList extends React.Component {
 
               <div class="col-md-4">
                 <div class="card mb-4 shadow-sm">
-                  <img src="/html/pix/mrMidnight_500px.jpg"  width="100%" height="100%" class = "img-responsive" ></img>
+                  <img src=""  width="100%" height="100%" class = "img-responsive" ></img>
                   <div class="card-body">
-                    <p class="card-text">Mr Midnight Dodges the Hunt: from The Gorgeous Bunch</p>
+                    <p class="card-text">Movie</p>
                     <div class="d-flex justify-content-between align-items-center">
                       <div class="btn-group">
                         <button type="button" class="btn btn-sm btn-secondary">Coming soon!</button>
@@ -138,19 +138,7 @@ export default class PersonList extends React.Component {
               </div>
            
 
-              <div class="col-md-4">
-                <div class="card mb-4 shadow-sm">
-                  <img src="/html/pix/drBabbit_500px.jpg"  width="100%" height="100%" class = "img-responsive" ></img>
-                  <div class="card-body">
-                    <p class="card-text">Dr Babbit Races to Lunch: from The Gorgeous Bunch</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-secondary">Coming soon!</button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              
               </div>
               </div>
               </div>
